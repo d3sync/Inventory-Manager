fc7346e729d1c695170e6ebdb8517711a3509022
d3sync <alex@idle.gr>
2022-07-16 13:59:53 +0300

    Add project files.

---
+++ Inventory Manager/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Inventory_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.αρχείοToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectToDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addJunkOnDBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.order_no = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Namem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.loadDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.αρχείοToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1128, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // αρχείοToolStripMenuItem
+            // 
+            this.αρχείοToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToDatabaseToolStripMenuItem,
+            this.loadDatabaseToolStripMenuItem});
+            this.αρχείοToolStripMenuItem.Name = "αρχείοToolStripMenuItem";
+            this.αρχείοToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.αρχείοToolStripMenuItem.Text = "Αρχείο";
+            // 
+            // connectToDatabaseToolStripMenuItem
+            // 
+            this.connectToDatabaseToolStripMenuItem.Name = "connectToDatabaseToolStripMenuItem";
+            this.connectToDatabaseToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.connectToDatabaseToolStripMenuItem.Text = "Connect to Database";
+            this.connectToDatabaseToolStripMenuItem.Click += new System.EventHandler(this.connectToDatabaseToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setupToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // setupToolStripMenuItem
+            // 
+            this.setupToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createDatabaseToolStripMenuItem,
+            this.addJunkOnDBToolStripMenuItem});
+            this.setupToolStripMenuItem.Name = "setupToolStripMenuItem";
+            this.setupToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.setupToolStripMenuItem.Text = "Setup";
+            // 
+            // createDatabaseToolStripMenuItem
+            // 
+            this.createDatabaseToolStripMenuItem.Name = "createDatabaseToolStripMenuItem";
+            this.createDatabaseToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.createDatabaseToolStripMenuItem.Text = "Create Database";
+            this.createDatabaseToolStripMenuItem.Click += new System.EventHandler(this.createDatabaseToolStripMenuItem_Click);
+            // 
+            // addJunkOnDBToolStripMenuItem
+            // 
+            this.addJunkOnDBToolStripMenuItem.Name = "addJunkOnDBToolStripMenuItem";
+            this.addJunkOnDBToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.addJunkOnDBToolStripMenuItem.Text = "Add Junk on DB";
+            this.addJunkOnDBToolStripMenuItem.Click += new System.EventHandler(this.addJunkOnDBToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 461);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1128, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.order_no,
+            this.Namem,
+            this.Address,
+            this.Status,
+            this.Price,
+            this.Date});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(651, 422);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // order_no
+            // 
+            this.order_no.HeaderText = "Order";
+            this.order_no.Name = "order_no";
+            // 
+            // Namem
+            // 
+            this.Namem.HeaderText = "Name";
+            this.Namem.Name = "Namem";
+            // 
+            // Address
+            // 
+            this.Address.HeaderText = "Address";
+            this.Address.Name = "Address";
+            // 
+            // Status
+            // 
+            this.Status.HeaderText = "Status";
+            this.Status.Name = "Status";
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "Price";
+            this.Price.Name = "Price";
+            // 
+            // Date
+            // 
+            this.Date.HeaderText = "Date";
+            this.Date.Name = "Date";
+            // 
+            // loadDatabaseToolStripMenuItem
+            // 
+            this.loadDatabaseToolStripMenuItem.Name = "loadDatabaseToolStripMenuItem";
+            this.loadDatabaseToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.loadDatabaseToolStripMenuItem.Text = "Load Database";
+            this.loadDatabaseToolStripMenuItem.Click += new System.EventHandler(this.loadDatabaseToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 483);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Πρόγραμμα Παραδόσεων";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem αρχείοToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createDatabaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectToDatabaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addJunkOnDBToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn order_no;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Namem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.ToolStripMenuItem loadDatabaseToolStripMenuItem;
+    }
+}
+
---
+++ Inventory Manager/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void createDatabaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           database xd = new database();
+            xd.initializeDB();
+            xd.populateDB();
+        }
+
+        private void connectToDatabaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection m_dbConnection;
+            m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+        }
+
+        private void addJunkOnDBToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection m_dbConnection;
+            m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+            string sql = "INSERT INTO Inventory (order_no, name , address, status, price, date) VALUES (123452367890,'logan2','herasde and now','0',1, 54.99,datetime('now')),(1232342323890,'logasdasan2','herasde aasdasdnd now','0',1, 34.99,datetime('now')),";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            m_dbConnection.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection conn;
+            conn = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            conn.Open();
+            string sql = "SELECT * FROM Inventory";
+            SQLiteCommand command = new SQLiteCommand(sql, conn);
+            using (SQLiteDataReader read = command.ExecuteReader())
+            {
+                while (read.Read())
+                {
+                    dataGridView1.Rows.Add(new object[] {
+            //read.GetValue(0),  // U can use column index
+            read.GetValue(read.GetOrdinal("order_no")),  // Or column name like this
+            read.GetValue(read.GetOrdinal("name")),
+            read.GetValue(read.GetOrdinal("address")),
+            read.GetValue(read.GetOrdinal("company_id")),
+            read.GetValue(read.GetOrdinal("del_status")),
+            read.GetValue(read.GetOrdinal("price"))
+            
+            });
+                }
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void loadDatabaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection conn;
+            conn = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            conn.Open();
+            string sql = "SELECT * FROM Inventory";
+            SQLiteCommand command = new SQLiteCommand(sql, conn);
+            using (SQLiteDataReader read = command.ExecuteReader())
+            {
+                while (read.Read())
+                {
+                    dataGridView1.Rows.Add(new object[] {
+                    //read.GetValue(0),  // U can use column index
+                    read.GetValue(read.GetOrdinal("order_no")),  // Or column name like this
+                    read.GetValue(read.GetOrdinal("name")),
+                    read.GetValue(read.GetOrdinal("address")),
+                    read.GetValue(read.GetOrdinal("company_id")),
+                    read.GetValue(read.GetOrdinal("del_status")),
+                    read.GetValue(read.GetOrdinal("price"))
+                    });
+                }
+            read.Close();
+            }
+        }
+    }
+}
